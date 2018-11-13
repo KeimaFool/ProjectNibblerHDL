b5f1fccbf9de993d81f83f7940764b71bf9b46c6
Sang Woo Shin <sangwooze@gmail.com>
2018-11-12 19:19:39 -0600

    added decoder and made Board a module not the testbench

--- Board.sv
+++ Board.sv
@@ -10,8 +10,9 @@
 `include "ROM.sv"
 `include "TristateBuffer.sv"
 `include "RAM.sv"
+`include "DecoderIO.sv"
 
-module Board ();
+module Board #(parameter N=4)(input logic clk, reset, input logic [N-1:0] In0,In1,In2, output logic C,Z, output logic [N-1:0] Out0,Out1,Out2,Accu);
 
 wire [15:0] control;
 wire [3:0] operand,ALUR,Aout;
@@ -20,14 +21,15 @@ wire [7:0] progbyte;
 wire [6:0] uRomAddress;
 wire [1:0] CarryZero;
 wire [3:0] databus;
-logic clk,reset;
+wire [15:0] outputE;
+wire [15:0] inputE;
 
 
 PC ProgCounter(.LOAD(loadAdd),.reset(reset),.loadE(~control[14]),.incPC(control[15]),.clk(clk),.INS(address));
 
 FFT Phase(.clk(clk),.reset(reset),.T(1'b1),.Q(uRomAddress[0]));
 
-FFD2 CZ(.clk(control[12]),.reset(reset),.D(~CarryZero),.Q(uRomAddress[2:1]));
+FFD2 CZ(.clk(control[12]),.reset(reset),.D(CarryZero),.Q(uRomAddress[2:1]));
 
 ControlROM uROM(.address(uRomAddress),.ProgOut(control));
 
@@ -45,39 +47,24 @@ TristateB TriALU(.tri_en(~control[3]),.entrada(ALUR),.salida(databus));
 
 RAM DRAM(.clk(clk), .we(~control[4]), .cs(~control[5]), .address(loadAdd),.data(databus));
 
+Decoder OutDecode(.load(~control[0]),.binary(operand),.onehot(outputE));
+
+Decoder InputDecode(.load(~control[2]),.binary(operand),.onehot(inputE));
+
+TristateB Input0(.tri_en(inputE[0]),.entrada(In0),.salida(databus));
+
+TristateB Input1(.tri_en(inputE[1]),.entrada(In1),.salida(databus));
+
+TristateB Input2(.tri_en(inputE[2]),.entrada(In2),.salida(databus));
+
+FFD4 Output0(.clk(outputE[0]),.reset(reset),.D(databus),.Q(Out0));
+
+FFD4 Output1(.clk(outputE[1]),.reset(reset),.D(databus),.Q(Out1));
+
+FFD4 Output2(.clk(outputE[2]),.reset(reset),.D(databus),.Q(Out2));
 
 assign loadAdd= {operand,progbyte};
-assign W=Aout;
-assign getAdd=address;
 
-initial begin
-    $display("\t\tTime\t\t clk \t phase \t\t progByte \t ALUR \t \t control \t Aout \t CZ \t loadAdd \t databus ");
-    $monitor("%d \t \t %b \t %b \t %b \t %d \t %b \t\t %d \t %b \t %d \t %d",$time, clk,uRomAddress,progbyte,address,control,Aout, CarryZero, loadAdd, databus);
-    //Initial values
-    clk=0;
-	reset=0;
-	
-	
-	
-    //Start
-	#1 reset=1;
-	#1 reset=0;
-	
-  end
-  
-  initial begin
-    #150 $finish;
-  end
-  
- always
-    #2 clk = ~clk;
-	
-  
-  initial 
-    begin
-  	  $dumpfile("dump.vcd");
-  	  $dumpvars(1);
-    end
 
 endmodule
 
---
+++ DecoderIO.sv
@@ -0,0 +1,38 @@
+/////////////////////////////
+//Sang Woo Shin 15372
+//Decoders for I/O
+//Procesador
+////////////////////////////
+
+module Decoder #(parameter N=4,M=16)(input logic load,input logic [N-1:0] binary, output logic [M-1:0] onehot);
+
+	always @(*)begin
+		if (load)begin
+			case(binary)
+				4'd0:  onehot=16'b0000000000000001;
+				4'd1:  onehot=16'b0000000000000010;
+				4'd2:  onehot=16'b0000000000000100;
+				4'd3:  onehot=16'b0000000000001000;
+				4'd4:  onehot=16'b0000000000010000;
+				4'd5:  onehot=16'b0000000000100000;
+				4'd6:  onehot=16'b0000000001000000;
+				4'd7:  onehot=16'b0000000010000000;
+				4'd8:  onehot=16'b0000000100000000;
+				4'd9:  onehot=16'b0000001000000000;
+				4'd10: onehot=16'b0000010000000000;
+				4'd11: onehot=16'b0000100000000000;
+				4'd12: onehot=16'b0001000000000000;
+				4'd13: onehot=16'b0010000000000000;
+				4'd14: onehot=16'b0100000000000000;
+				4'd15: onehot=16'b1000000000000000;
+				default: onehot=16'b0;
+			endcase
+		if (~load) begin
+			onehot=16'b0;
+		end
+		end
+	
+	end
+
+
+endmodule
